aea6e099e2210244333a4e594279d481d7e1b32c
Aryadip Mridha <mearjo1999@gmail.com>
2023-08-15 22:09:34 +0530

    day 4 start

---
+++ Day 4 -Randomisation/day-4-start.py
@@ -0,0 +1,16 @@
+import random
+import pi_val
+
+#generating random int
+random_integer=random.randint(1,10)
+print(random_integer)
+
+#modularising py functions
+print(f"value of pi is {pi_val.pi}")
+
+#random float from 0 to 5
+random_float=random.random()*5
+print(random_float)
+
+love_cal = random.randint(1,100)
+print(f"{love_cal} is you love value")
